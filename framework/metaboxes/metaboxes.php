5c6a71dcde99ae1df4da27e5b1e9fa1d8293f308
Stuart Starrs <stuart@waqastudios.com>
2018-08-23 14:47:43 +0200

    Hide Page Layout configuration from parent theme from pages that use the ACF template

---
+++ framework/metaboxes/metaboxes.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @package Inti
+ * @author Waqa Studios
+ * @since 1.0.0
+ * @version 1.0.8
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     https://github.com/WebDevStudios/CMB2
+ */
+
+
+/**
+ * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
+ */
+function inti_show_if_front_page( $cmb ) {
+	// Don't show this metabox if it's not the front page template
+	if ( $cmb->object_id !== get_option( 'page_on_front' ) ) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Conditionally hides a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
+ */
+function inti_hide_if_front_page( $cmb ) {
+	// Don't show this metabox if it IS the front page template
+	if ( get_post_meta($cmb->object_id, '_wp_page_template')[0] == 'page-templates/acf-page.php' ) {
+		//echo "<h1>xxx 1</h2>" .  get_post_meta($cmb->object_id, '_wp_page_template')[0];
+		return false;
+	} elseif  ( $cmb->object_id == get_option( 'page_on_front' ) ) {
+		//echo "<h1>xxx 2</h2>";
+		//var_dump(get_post_meta($cmb->object_id, '_wp_page_template'));
+		return false;
+	} 
+	return true;
+}
+
+add_action( 'cmb2_init', 'inti_register_layout_metabox' );
+/**
+ * Hook in and add a layout metabox. Can only happen on the 'cmb2_init' hook.
+ */
+function inti_register_layout_metabox() {
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_inti_layout_';
+
+	$cmb_layout = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => __( 'Configuration', 'inti' ),
+		'object_types'  => array( 'page', 'post' ), // Post type
+		'show_on_cb' => 'inti_hide_if_front_page', // function should return a bool value
+		'context'    => 'side',
+		'priority'   => 'core',
+		// 'show_names' => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // true to keep the metabox closed by default
+	) );	
+
+	$cmb_layout->add_field( array(
+		'name'    => __( 'Page Layout', 'inti' ),
+		'desc'    => __( 'Decide whether to show or hide a sidebar and where to place it.', 'inti' ),
+		'id'      => $prefix . 'radio',
+		'type'    => 'radio',
+		'default' => 'default',
+		'options' => inti_get_theme_layouts(true) // get theme layouts, with customizer default = true
+	) );
+	
+	if (current_theme_supports('inti-sticky-sidebars')) {
+		$cmb_layout->add_field( array(
+			'name'    => __( 'Sticky Sidebar', 'inti' ),
+			'desc'    => __( 'Keep sidebar widgets in view as you scroll down the page.', 'inti' ),
+			'id'      => $prefix . 'stickysidebars',
+			'type'    => 'radio',
+			'default' => 'default',
+			'options'          => array(
+				'default' => __( 'As Set In Customize', 'inti' ),
+				'static'   => __( 'Static', 'inti' ),
+				'sticky'     => __( 'Sticky', 'inti' ),
+			),
+		) );
+	}
+}
